59aa092dafe6baa9a9eeb8544723ab33824d63cd
happy most important <shin@cmc.is.i.nagoya-u.ac.jp>
2020-05-08 15:18:18 +0900

    basically accomplish code of the network part and now start editing training and validation codes

--- main.py
+++ main.py
@@ -1,9 +1,47 @@
 import torch
 import torch.nn as nn
 import opt
+import os
+import train
+import validation
+import torch.optim as optim
+from models import seg_resNext
+
+
+def generate_model(opts):
+    assert (opts.resnext in [50, 101, 152])
+    if opt.resnext == 50:
+        resnext_model = seg_resNext.resnet50(
+            opts.cardinality, opts.num_cat
+        )
+    if opt.resnext == 101:
+        resnext_model = seg_resNext.resnet101(
+            opts.cardinality, opts.num_cat
+        )
+    if opt.resnext == 152:
+        resnext_model = seg_resNext.resnet5152(
+            opts.cardinality, opts.num_cat
+        )
+
+def get_parameters(type,*args):
+    return 0
 
 if __name__ == '__main__':
     opts = opt.opt()
+    work_directory = opts.directory
+    annotation_directory  = os.path.join(work_directory,opts.annotation)
+    output_path = os.path.join(work_directory,opts.output)
+
+
+    resnext_model = generate_model(opts)
+    parameters = get_parameters()
+    optimizer = optim.SGD(parameters,lr=0.1,momentum=0.9,dampening=opts.dampening,nesterov=opts.nesterov)
+    lr_schedule = optim.lr_scheduler.ReduceLROnPlateau(optimizer,'min',patience=opts.patience,factor=opts.lr_decay)
+
+    for i in range(opts.epoches):
+        if not opts.no_train:
+
+
 
 
 
--- opt.py
+++ opt.py
@@ -4,6 +4,30 @@ import argparse
 def opt():
     parser = argparse.ArgumentParser(description='options for seg_net')
 
-    parser.add_argument()
+    parser.add_argument('--directory',required=True,type=str,help='the directory of dataset')
+
+    parser.add_argument('--annotation',required=True,type = str ,help=('the directory of annotation,regard as relative path to \'directory\' as default'))
+
+    parser.add_argument('--output',required=True,type=str,help='the output path of model and results of network running')
+
+    parser.add_argument('--resnext', default=101 ,type=int,help='the layers num of resnext,can be specified as [50,101,152]')
+
+    parser.add_argument('--cardinality',default=32,type=int,help='the cardinality of resnext, for more information please view the paper')
+
+    parser.add_argument('--epoches',default=50,type=int,help='epoches for training')
+
+    parser.add_argument('--lr_decay',default=0.1,type=float ,help='the fator of lr decaying')
+
+    parser.add_argument('--dampening',default=0 ,type = float,help = 'the dampening factor of SGD')
+
+    parser.add_argument('--nesterov' ,action='store_true',type=bool,help='use nesterov momentum in SGD or not')
+
+    parser.add_argument('--patience',default=10,type=int,help='the patience of lr_schedule which default use reducelronplateu')
+
+    parser.add_argument('--num_classes',default=400,type = int ,help ='set the categories num of dataset')
+
+    parser.add_argument('--no_train' ,action='store_false',type=bool,help='train or not')
+
+    parser.add_argument('--no_validation', action='store_false',type=bool,help ='validation or not')
 
     return parser.parse_args()
